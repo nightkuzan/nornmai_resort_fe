ad49ea5b6d6af76c3d260dcfa517e911544b0b30
unknown <p.ncry_1123@hotmail.com>
2022-09-26 13:09:36 +0700

    add check-in, check-out, search button

--- src/components/ReservComponent.js
+++ src/components/ReservComponent.js
@@ -1,15 +1,26 @@
-import { Component } from 'react'
+import { Component } from "react";
 
 class ReservComponent extends Component {
-
-    render() {
-        return(
-            <div className="container">
-                ROOMresver
-                <button>Search</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <h1>Reserve</h1>
+        <div className="form-group">
+          <label htmlFor="dob" className="control-label">
+            check-in
+          </label>
+          <input type="date" name="dob" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="dob" className="control-label">
+            check-out
+          </label>
+          <input type="date" name="dob" className="form-control" />
+        </div>
+        <button>search</button>
+      </div>
+    );
+  }
 }
 
-export default ReservComponent
+export default ReservComponent;
