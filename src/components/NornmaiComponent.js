f8853d33554d46a274fd5b461228d6e77a005db8
sunnytulakorn <sunnytulakorn@gmail.com>
2022-09-26 16:11:33 +0700

    create HistoryComponent

--- src/components/NornmaiComponent.js
+++ src/components/NornmaiComponent.js
@@ -1,4 +1,6 @@
 import { Component } from "react"
+import HistoryComponent from "./HistoryComponent";
+import ReservbookingComponent from "./ReservbookingComponent";
 import ReservComponent from "./ReservComponent";
 import RoomComponent from "./RoomComponent";
 class NornmaiComponent extends Component {
@@ -9,6 +11,7 @@ class NornmaiComponent extends Component {
                 <h1 className="centered">Welcome to Nornmai Resort</h1>
                 <RoomComponent/>
                 <ReservComponent/>
+                <HistoryComponent/>
             </div>
         )
     }
