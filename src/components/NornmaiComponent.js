eac55f984a093b155521ca304a9dac632a020c71
nightkuzan <78246688+nightkuzan@users.noreply.github.com>
2022-09-26 16:27:50 +0700

    merge

--- src/components/NornmaiComponent.js
+++ src/components/NornmaiComponent.js
@@ -10,12 +10,7 @@ class NornmaiComponent extends Component {
             <div className="bg-div">
                 <h1 className="centered">Welcome to Nornmai Resort</h1>
                 <RoomComponent/>
-<<<<<<< HEAD
                 {/* <ReservComponent/> */}
-=======
-                <ReservComponent/>
-                <HistoryComponent/>
->>>>>>> 0cdd7baaf6e5e7539be0fab81066bdc2e771af92
             </div>
         )
     }
