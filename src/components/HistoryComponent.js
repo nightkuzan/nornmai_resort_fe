adf25e8b2cc38e17395f99c596b9adfd5b2492c9
sunnytulakorn <sunnytulakorn@gmail.com>
2022-09-27 20:18:55 +0700

    show info on historyComponent

--- src/components/HistoryComponent.js
+++ src/components/HistoryComponent.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 import "./css/history.css";
 
 export default class HistoryComponent extends Component{
+    state = {}
+    constructor(){
+        super();
+        this.state ={
+            ctUserID: '',
+            mbTypeID: '',
+            ctPoint: 0,
+        };
+    }
+
+    componentDidMount() {
+        this.loginStorage = JSON.parse(localStorage.getItem('login'));
+        this.setState({ 'userId': <div className="column-g">User ID : {this.loginStorage.ctUserId}</div> });
+        this.setState({ 'memberType': <div className="column-g">Member Type : {this.loginStorage.mbTypeName}</div> })
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        };
+
+        fetch('http://localhost:3001/history?userid='+ this.loginStorage.ctUserId, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({ 'memberPoint': <div className="column-g">Member Point : {data.ctPoint}</div> })
+        })
+        .catch(error => {
+        console.error('There was an error!', error);
+        alert('เกิดข้อผิดพลาดในการเพิ่มข้อมูล');
+        });
+    }
     render(){
         return(
             <div className='container'>
@@ -10,9 +39,9 @@ export default class HistoryComponent extends Component{
                 </div>
                 <hr className='hr-line'></hr>
                 <div className='ht-info'>
-                    <div className='column-g'>User ID : </div>
-                    <div className='column-g'>Member Type : </div>
-                    <div className='column-g'>Member Point : </div>
+                    {this.state.userId}
+                    {this.state.memberType}
+                    {this.state.memberPoint}
                 </div>
             </div>
         )
