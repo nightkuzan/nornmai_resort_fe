c48876130cc403797284d5bbe63ae394c6ea4b27
Mrpopopopo <113658345+Mrpopopopo@users.noreply.github.com>
2022-09-29 00:05:48 +0700

    update reservbooking

--- src/components/ReservbookingComponent.js
+++ src/components/ReservbookingComponent.js
@@ -1,11 +1,39 @@
 import { Component } from 'react'
+import "./css/reservebooking.css";
+
 
 class ReservbookingComponent extends Component {
-
+    constructor(){
+        super();
+        this.state ={
+            ctUserID: '',
+            mbTypeID: '',
+            ctPoint: 0,
+        };
+    }
     render() {
         return(
             <div className="container">
-                ROOM
+               <div>
+                <header>Reserve Deluxe Room</header>
+               </div>
+               <div className='userDetail'>
+                    <div className='row1'>
+                        {this.state.userId}
+                        {this.state.memberType}
+                        {this.state.memberPoint}
+                    </div>
+                    <div className='row2'>
+                       <p1>โปรโมชั่น</p1>
+                        <input type="submit" value="GETDISCOUNT"></input>
+                        <p1>ใช้คะแนนเป็นส่วนลด10คะแนน = 1 บาท</p1>
+                        <input type="submit" value="๊USEPOINT"></input>
+                    </div>
+                    <div className='row3'>
+                        <header>SUMMARY</header>
+                    </div>
+               </div>
+               
             </div>
         )
     }
