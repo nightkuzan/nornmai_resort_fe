dc1c70f9b8e4d9a7b2d6a9f3f8c4864b3f1ee442
NATCHAREEYA PANYA <natchareeya_p@cmu.ac.th>
2022-09-28 16:55:21 +0700

    header-reserve, select-date checkin/out, button-search

--- src/components/ReservComponent.js
+++ src/components/ReservComponent.js
@@ -1,27 +1,62 @@
 import { Component } from "react";
 import { Buffer } from 'buffer';
 import moment from 'moment';
+import "./css/reserve.css";
+
 class ReservComponent extends Component {
-  
+  state = {}
+  constructor(){
+    super()
+    this.state ={
+      'room':[]
+    };
+  }
+  componentDidMount() {
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    };
+    fetch('http://localhost:3001/reserve', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          this.setState({ 'room':data })
+        })
+        .catch(error => {
+        console.error('There was an error!', error);
+        });
+  }
+  readImage(img){
+    var buffer = new Buffer(img,'base64');
+    return buffer
+  }
+
 
   render() {
     return (
-      <div className="container">
-        <h1>Reserve</h1>
-        <div className="form-group">
-          <label htmlFor="dob" className="control-label">
-            check-in
-          </label>
-          <input type="date" name="dob" className="form-control" />
+      <div className="row">
+        {/* {this.state.room.map((room,index)=>(
+          <div className="x" key={index}>
+          <img src={this.readImage(room.image)} alt="room"/>
+          </div>
+        ))} */}
+        
+          <div className='header-reserve'>
+              <header>RESERVE</header>
+          </div>
+        
+        <div className="select-date">
+          <div className="form-group">
+            <label htmlFor="dob" className="control-label">Check-in</label>
+            <input type="date" name="dob" value={this.state.dob} onChange={this.handleChange} className="form-control" />
+            <label htmlFor="dob" className="control-label">Check-out</label>
+            <input type="date" name="dob" value={this.state.dob} onChange={this.handleChange} className="form-control" />
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="dob" className="control-label">
-            check-out
-          </label>
-          <input type="date" name="dob" className="form-control" />
-        </div>
-        <button>search</button>
+        
+        <a className="button-search">SEARCH</a>
+        
       </div>
+      
     );
   }
 }
