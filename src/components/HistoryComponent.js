05b95f3c072c886a59699b816e3826aaf1741f25
sunnytulakorn <sunnytulakorn@gmail.com>
2022-10-03 17:28:14 +0700

    modify history component

--- src/components/HistoryComponent.js
+++ src/components/HistoryComponent.js
@@ -9,13 +9,25 @@ export default class HistoryComponent extends Component{
             ctUserID: '',
             mbTypeID: '',
             ctPoint: 0,
+            // bookingID: 0,
+            // roomType: '',
+            // checkIn: '',
+            // checkOut: '',
+            // DiscountCode: '',
+            // PointUse: 0,
+            // TotalPrice: 0,
+            // GetPoint: 0,
+            // Reason: '',
+            // BStatus: '',
+            // cancelReview: '',
+            room: []
         };
     }
     
 
     componentDidMount() {
         this.loginStorage = JSON.parse(localStorage.getItem('login'));
-        this.setState({ 'userId': <span className="column-g">User ID : {this.loginStorage.ctUserId}</span> });
+        this.setState({ 'userId': <span className="column-g">User ID : {this.showUserId(this.loginStorage.ctUserId)}</span> });
         this.setState({ 'memberType': <span className="column-g">Member Type : {this.loginStorage.mbTypeName}</span> })
         const requestOptions = {
             method: 'GET',
@@ -27,14 +39,25 @@ export default class HistoryComponent extends Component{
         .then(data => {
             this.setState({ 'memberPoint': <span className="column-g">Member Point : {data.ctPoint}</span> })
         })
-        // fetch('http://localhost:3001/booking?userid='+ this.loginStorage.ctUserId, requestOptions)
-        // .then(response => response.json())
-        // .then(data => {
-        //     this.setState({ 'memberPoint': <span className="column-g">Member Point : {data.ctPoint}</span> })
-        // }) fetch ข้อมูลในตาราง เดี๋ยวมาทำต่อ ร่างโครงไว้ก่อน
+
+        fetch('http://localhost:3001/history2?userid='+ this.loginStorage.ctUserId, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({ 'room': data });
+            console.log(data)
+        }) 
         .catch(error => {
         console.error('There was an error!', error);
         });
+
+    }
+    showUserId(id) {
+        let user = "CT";
+        for (let index = 0; index < 5 - id.toString().length; index++) {
+            user += "0";
+        }
+        user += id;
+        return user;
     }
 
     render(){
@@ -94,6 +117,27 @@ export default class HistoryComponent extends Component{
                             </th>
                         </tr>
                     </thead>               
+                <tbody>
+                {this.state.room.map((room,index)=>(
+                    <tr key={index} style={{ "verticalAlign": "middle"}}>
+                        <th scope="row">{index+1}</th>
+                        <td style={{textAlign: "center"}}>{room.bookingID}</td>
+                        <td style={{textAlign: "center"}}>{room.RoomTypeName}</td>
+                        <td style={{textAlign: "center"}}>{room.bkCheckInDate}</td>
+                        <td style={{textAlign: "center"}}>{room.rfloor}</td>
+                        <td style={{textAlign: "center"}}>{room.bkLeaveDate}</td>
+                        <td style={{textAlign: "center"}}>{room.dcCode}</td>
+                        <td style={{textAlign: "center"}}>{room.bkpointDiscount}</td>
+                        <td style={{textAlign: "center"}}>{room.bkTotalPrice}</td>
+                        <td style={{textAlign: "center"}}>{room.bkGetPoint}</td>
+                        <td style={{textAlign: "center"}}>{room.bkReason == null ? '-' : room.bkReason}</td>
+                        <td style={{textAlign: "center"}}>{room.bkStatus}</td>
+                        <td style={{ 'textAlign': 'center' }}>{room.reviewOpen === 'N' && room.status === 'NOT PAID' ?
+                                        <a href={"/cancelbooking?bookingid=" + room.bookingID}><button className="btn btn-danger">Cancel</button></a> :
+                                        room.reviewOpen === 'Y' ? <a href={"/review-room?bookingid=" + room.bookingID}><button className="btn btn-success">Review</button></a> : '-'}</td>
+                    </tr>
+                ))}
+                </tbody>
                 </table>
             </div>
         )
